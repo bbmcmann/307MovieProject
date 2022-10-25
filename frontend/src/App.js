c382000b44dc13d28d5fbb23fe3c3aa6e72f8849
Jonathan Laksana <jonathanalaksana@gmail.com>
2022-10-25 14:25:32 -0700

    add catch all routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,20 +14,37 @@ function App() {
             </div>
           }
         >
-          <Route index element={<p>home</p>} />
-          <Route path="movie" element={<p>movie page</p>}>
+          <Route index element={<p>home page</p>} />
+
+          {/* Movie paths */}
+          <Route path="movie">
+            <Route index element={<p>generic movie page</p>} />
             <Route path=":movieId" element={<p>movieId page</p>} />
+            <Route path="*" element={<h1>404 page not found</h1>} />
           </Route>
-          <Route path="review" element={<p>generic review page</p>} />
-          <Route path="review/:reviewId" element={<p>reviewId page</p>} />
-          <Route path="profile" element={<Outlet />}>
+
+          {/* Review paths */}
+          <Route path="review">
+            <Route index element={<p>generic review page</p>} />
+            <Route path=":reviewId" element={<p>reviewId page</p>} />
+            <Route path="*" element={<h1>404 page not found</h1>} />
+          </Route>
+
+          {/* Profile paths */}
+          <Route path="profile">
             <Route index element={<p>my profile page</p>} />
             <Route path=":id" element={<p>profile w id page</p>} />
             <Route path="edit" element={<p>edit profile page</p>} />
+            <Route path="*" element={<h1>404 page not found</h1>} />
           </Route>
+
+          <Route path="*" element={<h1>404 page not found</h1>} />
         </Route>
+
+        {/* Auth paths */}
         <Route path="login" element={<p>login page</p>} />
         <Route path="signup" element={<p>signup page</p>} />
+
         <Route path="*" element={<h1>404 page not found</h1>} />
       </Routes>
     </div>
