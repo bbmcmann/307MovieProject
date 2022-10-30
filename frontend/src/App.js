71e93cb5c37daf668658d8b3fe832a8fe2187c85
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-10-29 17:06:43 -0700

    Component Stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Header from './Header';
+import Home from './Components/Home';
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Home />
     </div>
 
   );
---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+import Pun from './Pun';
+
+
+function Home() {
+    return (
+        <div className="Home Page">
+            <Header />
+            <Pun />
+        </div>
+    );
+}
+
+export default Home;
