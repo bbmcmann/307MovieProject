35e87546d115e7b8ca31a03c725b2dc37adc481b
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-10-31 19:32:47 -0700

    Database Connection

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+let dbConnection;
+
+function getDbConnection() {
+    if (!dbConnection) {
+        dbConnection = mongoose.createConnection(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
+    return dbConnection;
+}
