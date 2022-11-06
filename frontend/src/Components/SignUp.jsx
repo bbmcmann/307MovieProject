c2929057d51ecd0a6d372caf6e02280284044453
Ben McMann <bbmcmann@volosys.com>
2022-11-05 20:10:13 -0700

    most style done

--- frontend/src/Components/SignUp.jsx
+++ frontend/src/Components/SignUp.jsx
@@ -1,44 +1,92 @@
 import Container from '@mui/material/Container';
-import styled from 'styled-components'
-
+import styled from 'styled-components'  
 
 const StyledCon = styled(Container)`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+  background: #D9D9D9;
+  margin: 30px;
+  padding: 20px;
+  border-radius: 10px;
+  width: 500px;
+`
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+
+const StyledLabel = styled.label`
+    display: flex;
+    flex-direction: column;
+    font-size: 25px;
+    color: #3E5336;
+    width: 100%;
+`
+
+const StyledInput = styled.input`
+    border: 1px solid 3E5336;
+    border-radius: 10px;
+    height: 33px;
+    padding-left: 10px;
+    font-size: 20px;
+    text-align: left;
+    margin-top: 5px;
+    margin-bottom: 20px;
+    margin-left: 5px;
+    margin-right: 5px;
+`
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    justify-content: space-between;
+`
+
+const StyledSubmit = styled.input`
+    background-color: #3E5336;
+    color: white;
+    border: 2px solid   #F6DA73;
+    border-radius: 10px;
+    height: 33px;
+    font-size: 20px;
 `
 
 function SignUp() {
-
     return (
-        <StyledCon>
-            <form>
-                <label>
-                    First Name:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Last Name:
-                    <input type="text" name="name" />
-                </label>
-                <label>
+        <StyledCon maxWidth="md">
+            <StyledForm>
+                <StyledDiv>
+                    <StyledLabel>
+                        First Name:
+                        <StyledInput type="text" name="name" />
+                    </StyledLabel>
+                    
+                    <StyledLabel>
+                        Last Name:
+                        <StyledInput type="text" name="name" />
+                    </StyledLabel>
+                </StyledDiv>
+                
+                <StyledLabel>
                     User Name:
-                    <input type="text" name="name" />
-                </label>
-                <label>
+                    <StyledInput type="text" name="name" />
+                </StyledLabel>
+                <StyledLabel>
                     Email:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Password:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Confirm Password:
-                    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+                    <StyledInput type="text" name="name" />
+                </StyledLabel>
+                <StyledDiv>
+                    <StyledLabel>
+                        Password:
+                        <StyledInput type="text" name="name" />
+                    </StyledLabel>
+                    <StyledLabel>
+                        Confirm Password:
+                        <StyledInput type="text" name="name" />
+                    </StyledLabel>
+                </StyledDiv>
+                <StyledSubmit type="submit" value="Submit" />
+            </StyledForm>
         </StyledCon>
     );
   }
