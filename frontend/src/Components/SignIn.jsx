9a4d2fface5aa9a4f248d22cfe3bd9447ea63d11
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-11-07 11:11:34 -0800

    Styling Form

--- frontend/src/Components/SignIn.jsx
+++ frontend/src/Components/SignIn.jsx
@@ -5,14 +5,11 @@ import { Box } from "@mui/material";
 
 const StyledBlock = styled(Container)`
 padding: 100px;
+margin: 75px;
 justify-content: center;
 background: #f6da73;
 box-shadow: 5px 5px 5px gray;
-position: absolute;
 width: 500px;
-height: 550px;
-left: 175px;
-top: 100px;
 `
 
 const StyledHead = styled.div`
@@ -24,22 +21,22 @@ text-align: center;
 `
 
 const StyledText = styled.div`
-fint-style: normal;
+font-style: normal;
 font-size: 30px;
 padding-top: 25px;
 padding-bottom: 30px;
 `
 const StyledInput = styled.input`
 border: 1px solid 3E5336;
-    border-radius: 10px;
-    height: 33px;
-    padding-left: 10px;
-    font-size: 20px;
-    text-align: left;
-    margin-top: 5px;
-    margin-bottom: 20px;
-    margin-left: 5px;
-    margin-right: 5px;
+border-radius: 10px;
+height: 33px;
+padding-left: 10px;
+font-size: 20px;
+text-align: left;
+margin-top: 5px;
+margin-bottom: 20px;
+margin-left: 5px;
+margin-right: 5px;
 `
 
 // add effects to button
@@ -48,13 +45,11 @@ type: submit;
 display: inline-block;
 border: none;
 font-size: 25px;
-border-radius: 3px;
 padding: 0.5rem 0;
 width: 11rem;
 height: 50px;
 background-color: #3E5336;
 color: white;
-border: 2px solid white;
 `
 
 function SignIn() {
@@ -84,27 +79,29 @@ function SignIn() {
 
     return (
 
-            <StyledBlock>
-                <StyledHead>The Bananalyst</StyledHead>
-                <StyledText>Login </StyledText>
-                <StyledInput 
-                    type="text"
-                    name="username"
-                    id="username"
-                    placeholder="Username"
-                    onChange={handleChange}
-                />
-                <br></br>
-                <StyledInput 
-                    type="text"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                />
-                <br></br>
-                <StyledButton>Login</StyledButton>
-                <p>Don't have an account? Sign up here</p>
+            <StyledBlock maxWidth="md">
+                <form>
+                    <StyledHead>The Bananalyst</StyledHead>
+                    <StyledText>Login </StyledText>
+                    <StyledInput 
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="Username"
+                        onChange={handleChange}
+                    />
+                    <br></br>
+                    <StyledInput 
+                        type="text"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                    />
+                    <br></br>
+                    <StyledButton>Login</StyledButton>
+                    <p>Don't have an account? Sign up here</p>
+                </form>
             </StyledBlock>
 
     )
