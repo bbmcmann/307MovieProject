a8aa2a1c405dc0169f8ccc7c67269c8ac563c256
Ben McMann <bbmcmann@volosys.com>
2022-11-13 19:04:33 -0800

    port

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log("REST API is listening.");
 });
