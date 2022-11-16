5f86baac1945d8f19945e6832c9480131e292cec
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-11-16 13:23:17 -0800

    Button to favorite pun

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Button } from "@mui/material";
-import axios from "axios";
+import React from "react";
 import Pun from "./Pun";
 
 function Landing() {
--- frontend/src/components/Pun.jsx
+++ frontend/src/components/Pun.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import { Button } from "@mui/material";
-import axios from "axios";
 
 function Pun() {
   let pun_q = [
