50550cae6c5d2796548242aeade71bd29c1f8849
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-12-02 16:58:44 -0800

    Using function in useState

--- frontend/src/components/profile/ProfileLiked.jsx
+++ frontend/src/components/profile/ProfileLiked.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../../styles/Profile.css";
 
 function ProfileLiked({ reviews }) {
-  let movies = [];
+  const movies = useState(handleMovieList(reviews));
   const navigate = useNavigate();
 
   async function getMovie(movie_id) {
@@ -12,11 +12,23 @@ function ProfileLiked({ reviews }) {
       const res = await axios.get(
         `${process.env.REACT_APP_BACKEND_URL}movies/${movie_id}`
       );
-      movies.push(res.data);
-      return true;
+      const movie = res.data;
+      console.log(movie);
+      return movie;
     } catch (error) {
       console.log(error);
       return false;
+    }
+  }
+
+  function handleMovieList(reviews) {
+    if (reviews) {
+      let movie_list = [];
+      reviews = reviews.filter((review) => review.ratingVal >= 5);
+      reviews.forEach((review) => movie_list.push(getMovie(review.movie_id)));
+      return movie_list;
+    } else {
+      console.log("No Reviews");
     }
   }
 
@@ -32,9 +44,10 @@ function ProfileLiked({ reviews }) {
     <div className="liked-section">
       <h3>Liked Movies :</h3>
       {reviews ? (
-        ((reviews = reviews.filter((rev) => rev.ratingVal >= 5)),
+        ((reviews = reviews.filter((review) => review.ratingVal >= 5)),
         (reviews.forEach((review) => getMovie(review.movie_id)),
         (
+          // handleMovieList(),
           <div className="movies">
             {movies?.map((movie) => {
               console.log(movies);
@@ -44,7 +57,7 @@ function ProfileLiked({ reviews }) {
                   className="poster"
                   src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
                   alt="movie poster"
-                  onClick={() => handleClick(movie._id)}
+                  onClick={() => handleClick(movie.id)}
                 />
               );
             })}
