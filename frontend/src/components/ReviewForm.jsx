c3c4ed10f96b2df079cfec65875eb75f75e22c96
Jonathan Laksana <jonathanalaksana@gmail.com>
2022-12-02 23:43:59 -0800

    fix update reviewlist

--- frontend/src/components/ReviewForm.jsx
+++ frontend/src/components/ReviewForm.jsx
@@ -1,11 +1,12 @@
-import axios from "axios";
 import { Box, Button, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Rating from "@mui/material/Rating";
 import TextField from "@mui/material/TextField";
+import axios from "axios";
 import React, { useState } from "react";
-import styled from "styled-components";
 import { Cookies } from "react-cookie";
+import styled from "styled-components";
+import getBackendUrl from "./util";
 // import "./Movie.css";
 
 // function setValue=
@@ -51,32 +52,28 @@ function ReviewForm(props) {
   }
 
   async function handleSubmit(rating) {
-    console.log(rating);
     const auth_id = cookies.get("userId");
     const poster_name = cookies.get("username");
-    console.log("poster_name");
-    console.log(poster_name);
-    console.log(auth_id);
     const config = {
       headers: { Authorization: `Bearer ${cookies.get("token")}` },
     };
+    const newRev = {
+      title: rating.title,
+      review: rating.review,
+      ratingVal: rating.ratingVal,
+      user_name: poster_name,
+      user_id: auth_id,
+      movie_id: props.id,
+      date_posted: new Date(),
+      upvote_list: [],
+      downvote_list: [],
+    };
     axios
-      .post(
-        `${process.env.REACT_APP_BACKEND_URL}reviews`,
-        {
-          title: rating.title,
-          review: rating.review,
-          ratingVal: rating.ratingVal,
-          user_name: poster_name,
-          user_id: auth_id,
-          movie_id: props.id,
-          date_posted: new Date(),
-          upvote_list: [],
-          downvote_list: [],
-        },
-        config
-      )
-      .then((res) => console.log(res))
+      .post(`${getBackendUrl()}reviews`, newRev, config)
+      .then((res) => {
+        console.log(res);
+        props.update(newRev);
+      })
       .catch((err) => {
         console.log(err.response.data);
         console.log(err.response.status);
