cdabdf23bb3a7e7d1fa4da8d654b24b4c04f506a
pullingrm12 <76963973+pullingrm12@users.noreply.github.com>
2022-12-02 18:30:25 -0800

    Try using useEffect()

--- frontend/src/components/profile/ProfileLiked.jsx
+++ frontend/src/components/profile/ProfileLiked.jsx
@@ -1,11 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../../styles/Profile.css";
 
 function ProfileLiked({ reviews }) {
-  const movies = useState(handleMovieList(reviews));
+  const [movies, setMovies] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (reviews) {
+      function handle(movie_list) {
+        return new Promise(async function (resolve, reject) {
+          try {
+            setTimeout(() => {
+              resolve(movie_list);
+            }, 3000);
+          } catch (error) {
+            reject(console.log(error));
+          }
+        });
+      }
+      async function handleMovieList() {
+        let movie_list = [];
+        let revs = reviews.filter((review) => review.ratingVal >= 5);
+        await revs.forEach((review) =>
+          movie_list.push(getMovie(review.movie_id))
+        );
+        return await handle(movie_list);
+      }
+      handleMovieList()
+        .then((result) => {
+          console.log(result);
+          setMovies(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("No reviews");
+    }
+  }, [reviews]);
 
   async function getMovie(movie_id) {
     try {
@@ -13,22 +47,10 @@ function ProfileLiked({ reviews }) {
         `${process.env.REACT_APP_BACKEND_URL}movies/${movie_id}`
       );
       const movie = res.data;
-      console.log(movie);
       return movie;
     } catch (error) {
       console.log(error);
       return false;
-    }
-  }
-
-  function handleMovieList(reviews) {
-    if (reviews) {
-      let movie_list = [];
-      reviews = reviews.filter((review) => review.ratingVal >= 5);
-      reviews.forEach((review) => movie_list.push(getMovie(review.movie_id)));
-      return movie_list;
-    } else {
-      console.log("No Reviews");
     }
   }
 
@@ -44,25 +66,23 @@ function ProfileLiked({ reviews }) {
     <div className="liked-section">
       <h3>Liked Movies :</h3>
       {reviews ? (
-        ((reviews = reviews.filter((review) => review.ratingVal >= 5)),
-        (reviews.forEach((review) => getMovie(review.movie_id)),
-        (
-          // handleMovieList(),
-          <div className="movies">
-            {movies?.map((movie) => {
-              console.log(movies);
-              return (
-                <img
-                  key={movie._id}
-                  className="poster"
-                  src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
-                  alt="movie poster"
-                  onClick={() => handleClick(movie.id)}
-                />
-              );
-            })}
-          </div>
-        )))
+        //((reviews = reviews.filter((review) => review.ratingVal >= 5)),
+        //(reviews.forEach((review) => getMovie(review.movie_id)),
+        // handleMovieList(),
+        <div className="movies">
+          {movies?.map((movie) => {
+            console.log(movies);
+            return (
+              <img
+                key={movie._id}
+                className="poster"
+                src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+                alt="movie poster"
+                onClick={() => handleClick(movie.id)}
+              />
+            );
+          })}
+        </div>
       ) : (
         <h3>--None--</h3>
       )}
